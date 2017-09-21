863edab4a1a27c5f2b37807765baecb5a3822c15
conni <c.buschka@arcusx.com>
2017-09-21 23:09:19 +0200

    Now build-and-run.sh uses $HOME/.m2/repository.

--- integration-test-build/build-and-run.sh
+++ integration-test-build/build-and-run.sh
@@ -6,7 +6,7 @@ BASE_DIR=$(cd `dirname $0` && pwd)
 
 docker build --tag agfsapi4j-integration-test-build:local -f $BASE_DIR/Dockerfile $BASE_DIR
 
-CMD='mvn clean package -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests -Dmaven.repo.local=/work/.m2/repository/'
+CMD='mvn clean package -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests'
 if [ $# -ne 0 ]; then
   CMD="$*"
 fi
@@ -15,6 +15,7 @@ docker run -ti --rm -e DEBUG_ENTRYPOINT=1 \
 	-e WORKER_UID=$(id -u) \
 	-e WORKER_GID=$(id -g) \
 	-v $PROJECT_DIR/:/work \
+	-v $HOME/.m2/repository/:/home/worker/.m2/repository/ \
 	--privileged \
 	agfsapi4j-integration-test-build:local \
 	"$CMD" 
