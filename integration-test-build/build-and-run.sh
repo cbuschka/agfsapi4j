e31552be5993c5154d6f30c00f9f96ca0bd3dc9f
conni <c.buschka@arcusx.com>
2017-09-22 11:55:28 +0200

    sonar.login now sys prop on mvn command.

--- integration-test-build/build-and-run.sh
+++ integration-test-build/build-and-run.sh
@@ -15,22 +15,18 @@ ls -la $(dirname $BUILD_IMAGE_CACHE_FILE)/
 if [ -f "$BUILD_IMAGE_CACHE_FILE" ]; then
   docker load -i $BUILD_IMAGE_CACHE_FILE
 else
-  rm $(dirname $BUILD_IMAGE_CACHE_FILE)/*.docker.tar || true
+  rm $(dirname $BUILD_IMAGE_CACHE_FILE)/*.docker.t?? || true
   docker build --tag $BUILD_IMAGE_FQNAME -f $BASE_DIR/Dockerfile $BASE_DIR
   mkdir -p $(dirname $BUILD_IMAGE_FQNAME) && \
   docker save -o $BUILD_IMAGE_CACHE_FILE $BUILD_IMAGE_FQNAME
 fi
 
-CMD='mvn -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests,sonar-upload clean verify'
+CMD="mvn -Dsonar.login=$SONAR_TOKEN -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests,sonar-upload clean verify"
 if [ $# -ne 0 ]; then
   CMD="$*"
 fi
 
-echo "Command: $CMD"
-echo "sha256 of salted SONAR_TOKEN: $(echo "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b$SONAR_TOKEN" | sha256sum)"
-
 docker run -ti --rm -e DEBUG_ENTRYPOINT=1 \
-	-e SONAR_TOKEN=$SONAR_TOKEN \
 	-e WORKER_UID=$(id -u) \
 	-e WORKER_GID=$(id -g) \
 	-v $PROJECT_DIR/:/work \
