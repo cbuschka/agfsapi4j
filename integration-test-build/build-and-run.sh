3be372bf03c3ce3cd9361313882f909a934cd8b9
conni <c.buschka@arcusx.com>
2017-09-22 09:10:37 +0200

    Added caching of docker image.

--- integration-test-build/build-and-run.sh
+++ integration-test-build/build-and-run.sh
@@ -1,10 +1,20 @@
-#!/bin/bash 
+#!/bin/bash -xe
 
 PROJECT_DIR=$(cd `dirname $0`/.. && pwd)
 
 BASE_DIR=$(cd `dirname $0` && pwd)
 
-docker build --tag agfsapi4j-integration-test-build:local -f $BASE_DIR/Dockerfile $BASE_DIR
+BUILD_IMAGE_CACHE_FILE=$HOME/.cache/docker/agfsapi4j-integration-test-build.docker.tar
+BUILD_IMAGE_FQNAME=agfsapi4j-integration-test-build:local
+
+if [ -f "$BUILD_IMAGE_CACHE_FILE" ]; then
+  docker load -i $BUILD_IMAGE_CACHE_FILE
+fi
+
+docker build --tag $BUILD_IMAGE_FQNAME -f $BASE_DIR/Dockerfile $BASE_DIR
+
+mkdir -p $(dirname $BUILD_IMAGE_FQNAME) && \
+docker save -o $BUILD_IMAGE_CACHE_FILE $BUILD_IMAGE_FQNAME
 
 CMD='mvn -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests,sonar-upload clean verify'
 if [ $# -ne 0 ]; then
@@ -19,5 +29,5 @@ docker run -ti --rm -e DEBUG_ENTRYPOINT=1 \
 	-v $PROJECT_DIR/:/work \
 	-v $HOME/.m2/repository/:/home/worker/.m2/repository/ \
 	--privileged \
-	agfsapi4j-integration-test-build:local \
+	$BUILD_IMAGE_FQNAME \
 	"$CMD" 
