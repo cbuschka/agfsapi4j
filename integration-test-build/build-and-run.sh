c535fd2456ee237cf3aec3fb65748ab6be0a3914
conni <c.buschka@arcusx.com>
2017-09-22 10:53:55 +0200

    Improved docker image caching.

--- integration-test-build/build-and-run.sh
+++ integration-test-build/build-and-run.sh
@@ -4,17 +4,20 @@ PROJECT_DIR=$(cd `dirname $0`/.. && pwd)
 
 BASE_DIR=$(cd `dirname $0` && pwd)
 
-BUILD_IMAGE_CACHE_FILE=$HOME/.cache/docker/agfsapi4j-integration-test-build.docker.tar
 BUILD_IMAGE_FQNAME=agfsapi4j-integration-test-build:local
+
+cd $BASE_DIR
+BUILD_IMAGE_CHECKSUM=$(tar c assets/ Dockerfile | md5sum | cut -d' ' -f 1)
+BUILD_IMAGE_CACHE_FILE=$HOME/.cache/docker/agfsapi4j-integration-test-build.$BUILD_IMAGE_CHECKSUM.docker.tar
 
 if [ -f "$BUILD_IMAGE_CACHE_FILE" ]; then
   docker load -i $BUILD_IMAGE_CACHE_FILE
+else
+  rm $(dirname $BUILD_IMAGE_CACHE_FILE)/*.docker.tar || true
+  docker build --tag $BUILD_IMAGE_FQNAME -f $BASE_DIR/Dockerfile $BASE_DIR
+  mkdir -p $(dirname $BUILD_IMAGE_FQNAME) && \
+  docker save -o $BUILD_IMAGE_CACHE_FILE $BUILD_IMAGE_FQNAME
 fi
-
-docker build --tag $BUILD_IMAGE_FQNAME -f $BASE_DIR/Dockerfile $BASE_DIR
-
-mkdir -p $(dirname $BUILD_IMAGE_FQNAME) && \
-docker save -o $BUILD_IMAGE_CACHE_FILE $BUILD_IMAGE_FQNAME
 
 CMD='mvn -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests,sonar-upload clean verify'
 if [ $# -ne 0 ]; then
