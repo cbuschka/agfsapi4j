57037d66f9f4fd1d96e04f825837a53f335c92a0
conni <c.buschka@arcusx.com>
2017-09-22 08:58:16 +0200

    Added sonar plugin to pom.xml.

--- integration-test-build/build-and-run.sh
+++ integration-test-build/build-and-run.sh
@@ -6,10 +6,12 @@ BASE_DIR=$(cd `dirname $0` && pwd)
 
 docker build --tag agfsapi4j-integration-test-build:local -f $BASE_DIR/Dockerfile $BASE_DIR
 
-CMD='mvn clean package -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests sonar:sonar'
+CMD='mvn -Dcom.github.agfsapi4j.it.hostname=\$HOSTNAME -Pintegration-tests,sonar-upload clean verify'
 if [ $# -ne 0 ]; then
   CMD="$*"
 fi
+
+echo "Command: $CMD"
 
 docker run -ti --rm -e DEBUG_ENTRYPOINT=1 \
 	-e WORKER_UID=$(id -u) \
